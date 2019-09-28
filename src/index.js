f9ff76a6b94b0567c5bbc42a7c77af34f21cb163
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-28 15:48:02 -0700

    cleaning up code

--- src/index.js
+++ src/index.js
@@ -17,49 +17,6 @@ app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
-        userId: req.context.me.id,
-    };
-
-    req.context.models.messages[id] = message;
-
-    return res.send(message);
-});
-
-app.delete('/messages/:messagesId', (req, res) => {
-    const {
-        [req.params.messageId] : message,
-        ...otherMessages
-    } = req.context.model.messages;
-
-    req.context.model.messages = otherMessages;
-    res.send(messages);
-});
-
 app.use((req, res, next) => {
     req.context = {
         models,
