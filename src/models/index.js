e3220963212bc2e2e51ff66cc3f42ca3a0dfe1ca
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-28 14:44:49 -0700

    creating first model

---
+++ src/models/index.js
@@ -0,0 +1,23 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'Hassan',
+    },
+    2: {
+        id: '2',
+        username: 'Ahmed',
+    },
+};
+
+let messages= {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Goodbye World',
+        userId: '2'
+    },
+};
