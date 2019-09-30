62640ad4f42ac4b6b7ea5e048b7c7a1b1452d4cf
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-29 22:51:34 -0700

    importing user

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,3 @@
 import mongoose from 'mongoose';
 
-let users = {
-    1: {
-        id: '1',
-        username: 'Hassan',
-    },
-    2: {
-        id: '2',
-        username: 'Ahmed',
-    },
-};
-
-let messages= {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'Goodbye World',
-        userId: '2'
-    },
-};
+import User from './user';
