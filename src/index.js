c3b1904d253bc523e74492b96f811e692607b31f
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-15 15:22:40 -0700

    making changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ : true }));
 
 app.use('/session', routes.session);
 app.use('/users', routes.user);
