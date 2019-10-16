889b8a42315cbf3b9aa7d1afb0899c3f2814be62
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-16 10:19:33 -0700

    finished creating routes

--- src/routes/message.js
+++ src/routes/message.js
@@ -16,7 +16,24 @@ router.get('/:messageId', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const message = await req.context
+    const message = await req.context.models.Message.create({
+        text: req.body.text, 
+        user:req.context.me.id,
+    });
+    return res.send(message);
+});
+
+router.delete('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findById(
+        req.params.messageId,
+    );
+
+    let result = null;
+    if (message) {
+        result = await message.remove();
+    }
+
+    return res.send(result);
 });
 
 export default router;
